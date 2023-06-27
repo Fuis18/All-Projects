2bfc3ce9a0d9014497c7e0c122091d0fd5eb5cef
Fuis18 <96286214+Fuis18@users.noreply.github.com>
2023-06-26 23:06:38 -0500

    Update js.js

--- Pages/1/js.js
+++ Pages/1/js.js
@@ -1,9 +1,14 @@
 'use strict';
 let num1 = 1, num2 = 1, num3 = 1;
-document.querySelector(".f1__answer button").addEventListener("click",()=>{
-   if (document.querySelector(".f1__input1-left").value != '') num1 = document.querySelector(".f1__input1-left").value;
-   if (document.querySelector(".f1__input1-right").value != '') num2 = document.querySelector(".f1__input1-right").value;
-   if (document.querySelector(".f1__input1-down").value != '') num3 = document.querySelector(".f1__input1-down").value;
-   let res = (num1 * num2) / num3;
-   document.querySelector(".result1").textContent = "Resultado: " + res;
-});
+let left = document.querySelector(".f1__input1-left");
+let right = document.querySelector(".f1__input1-right");
+let down = document.querySelector(".f1__input1-down");
+document.querySelectorAll(".f1__container input").forEach((input) => {
+   input.addEventListener("keyup",(e) => {
+      if (left.value != '') num1 = left.value;
+      if (right.value != '') num2 = right.value;
+      if (down.value != '') num3 = down.value;
+      let res = (num1 * num2) / num3;
+      document.querySelector(".result1").textContent = "Resultado: " + res;
+   })
+})
